3e8f1b4f8b015001dc16ddfd893798da4368d887
websiteaap <53229705+websiteaap@users.noreply.github.com>
2019-11-24 17:20:41 +0530

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+<link rel="stylesheet" type="text/css" >
+</head>
+<body>
+<div id="one">
+	
+	<form action="processre.php" method="post">
+<h1>register</h1>
+	<p><label>Username</label>
+<input type="text" name="user" name="user" placeholder="Enter Username" />
+</p>
+	
+<p>
+	<label>password</label>
+<input type="password" name="pass"  placeholder="Enter Password" />
+</p>
+	<p><label>Re-Enter password</label>
+<input type="password" name="pass2" placeholder="Enter Password" />
+</p>
+<p>
+	
+<input type="submit" name="btn" id="three"  value="register" />
+</p>
+</form>
+	/<div>
+</body>
+<style type="text/css">
+	#one{
+		border:10px ridge red;
+		padding-right: 30px;
+		padding-right: 50px;
+		padding-top: 50px;
+		padding-left: 50px;
+		padding-bottom: 10px;
+		margin-top: 100px;
+		width: 500px;
+		margin-left: 27em;
+background-image: url('aaaa.jpg');
+background-size: cover;
+	}
+	body{text-align: center;
+	}
+	h1{
+		text-transform: uppercase;
+		color: red;
+		margin-left: 100px;
+		padding-bottom: 40px;
+background-image: url('11331.jpg');	
+background-size: cover;
+border-radius: 50px;
+width: 290px;
+height: 50px;
+line-height: 90px;
+}
+p{
+	color: yellow;
+	text-transform: uppercase;
+	font-size: 25px;
+}
+#three{
+	background-color: darkblue;
+	font-size: 25px;
+	color: yellow;
+	border-radius: 20px;
+}
+</style>
+</html>
